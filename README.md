b4a2c309ed23b7e06aaee4274a5662c111603cb4
Nat Standridge <standridgen@gmail.com>
2023-02-21 21:50:24 -0700

    Integrated the two new classes and got everything working with the changes from the other branch. Also made some changes to both classes, as well as the main function necessary to get things working smoothly.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 ## A way to map the subdomains used by scammers
 
 ## To-Do:
-1. Make a function that takes in an arbitrary scammer link, sets parameters based on number of integers/URL format
-    - find and remove blocks of integers, replace with special token
-2. Make a CLI that lets a user enter a scam URL and automatically start mapping their subdomains
+1. Add an actual visual and interactive map wtih tkinter or similar
--- main.py
+++ main.py
@@ -3,22 +3,19 @@ import multiprocessing as mp
 from multiprocessing import Process, Queue, Lock
 import numpy as np
 import argparse
+import time
 
 class DigitBlockParser:
 	''' this class parses input strings and returns information
 	about blocks of digits in the string'''
 
-	def analyze_string(self, s: str, min_block_len: int, token: str) -> str:
+	def analyze_string(self, s: str, min_block_len: int) -> str:
 		base_char = ""
 		in_block = False
 		block = ""
 		blocks = []
 		s += "\n"
-		print(f"str: {s}")
 		for char in s:
-			if in_block: status_str = "in int block"
-			else: status_str = "out of block"
-			print(f"{status_str}: {block}")
 			if str(char).isdigit():
 				block += char
 				if not in_block:
@@ -35,33 +32,39 @@ class DigitBlockParser:
 		blocks.sort(key=len)
 
 		for b in blocks:
-			s = s.replace(b, token)
+			s = s.replace(b, '{int(num)}')
 		
-		# not sure why you need to remove two here... is there another newline added or something?
-		s = s[:-2]
+		s = s[:-1]
 
-		return blocks, s
+		return blocks, f'f"""{s}"""' ## f string hack
 
-class Mapper:
+
+class Mapper(DigitBlockParser):
 	def __init__(self):
 		self.queue = Queue()
 		self.lock = Lock()
 
 	def writer(self):
 		self.lock.acquire()
-		with open('good_domains.txt', 'a') as f:
+		with open('verified_domains.txt', 'a') as f:
 			url = self.queue.get()
 			f.write(url + '\n')
 		f.close()
 		self.lock.release()
 		print(f"Writer process is finished writing {url}")
 
-	def checker(self, start_num, end_num):
+	def checker(self, url_f_string, start_num, end_num):
+		''' 
+		Takes in url_f_string, start_num, and end_num to run through every possible URL combo and run the writer if it has data.
+		
+		'''
 		num = start_num
 		while num < end_num:
-			url = f"http://s{int(num)}.onlinehome.us"
+			url = url_f_string
+			url = eval(url) ## evaluating to insert num into url_f_string
 			try:
 				request = str(requests.get(url))
+				time.sleep(3)
 				
 				if '20' in request:
 					self.queue.put(url)
@@ -73,32 +76,55 @@ class Mapper:
 				else:
 					print(f'URL: {url} is not valid and did not return 300-level code.')
 
-			except Exception as e:
+			except requests.exceptions.RequestException as e: ## have to ignore request exceptions to pass on pages that are timing out
+				#print(e)
 				print(f"URL: {url} is not valid.")
 				
 			num += 1
 				
-	def main(self):
+def main(num_processes=10):
 
-		parser = argparse.ArgumentParser()
-		parser.add_argument("-u", nargs=1, type=str) ## -u flag followed by URL
-		args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-u", nargs=1, type=str) ## -u flag followed by URL
+	args = parser.parse_args()
+
+	if getattr(args, "u") == None:
+		url = input("\nFormat should be http:// or https://scamurl1423523.com\nPlease enter the scam URL you want to map: ")
+
+	else:
 		url = str(getattr(args, "u"))
-		url = url.replace("'", "").replace("[","").replace("]","")
+		url = url.replace("'", "").replace("[","").replace("]","") ## cleaning string because it retains tokens from list
 
-		num_processes = 10
-		processes = []
-		
-		num_range = np.linspace(100000000, 1000000000, num_processes + 1)
-		
-		for i in range(num_processes):
-			proc = Process(target=self.checker, args=[num_range[i], num_range[i+1]])
-			proc.start()
-			processes.append(proc)
-		
-		for p in processes:
-			p.join()
+	dbp = DigitBlockParser()
+	num_block, url_f_string = dbp.analyze_string(url, 3) ## gets block of numbers larger than 3 out of url, and creates f string based on url with numbers removed (for iterating)
+	num_block = str(num_block).replace("'", "").replace("[","").replace("]","") ## have to remove tokens left over from list
+
+	num_of_nums = len(str(num_block)) ## this is the number of digits we need to start with (num_of_nums + 1 will be the end_num)
+
+	start_num_string = '1'
+	end_num_string = '1'
+
+	for i in range(num_of_nums-1):
+		start_num_string += '0'
+
+	for i in range(num_of_nums):
+		end_num_string += '0'
+
+	start_num, end_num = int(start_num_string), int(end_num_string)
+
+	processes = []
+	
+	num_range = np.linspace(start_num, end_num - 1, num_processes + 1)
+
+	mapper = Mapper()
+	
+	for i in range(num_processes):
+		proc = Process(target=mapper.checker, args=[url_f_string, num_range[i], num_range[i+1]])
+		proc.start()
+		processes.append(proc)
+	
+	for p in processes:
+		p.join()
 
 if __name__ == '__main__':
-	mapper = Mapper()
-	mapper.main()
+	main()
